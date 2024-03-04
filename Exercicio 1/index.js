0c3f4595b2167eb87a3c54bb45ab493ac9e092d0
Rafael <rafael06092001@gmail.com>
2024-03-04 20:56:56 -0300

    Exercicio 1 concluido

--- Exercicio 1/index.js
+++ Exercicio 1/index.js
@@ -1,1 +1,23 @@
-console.log("Teste")
+let data = new Date()
+let horas = data.getHours()
+
+console.log(horas)
+// Alterar parágrafo com horário
+const horario = document.getElementById("horario")
+
+horario.innerText = `Agora são ${horas} horas`;
+
+//Alterar fundo e a imagem - Tarde = rgba(53,47,73,255)
+let bodyColor = document.body.style
+let imagem = document.getElementById("imagemPrincipal")
+
+if (horas >= 5 && horas <= 12) {
+    bodyColor.backgroundColor = "rgb(236, 204, 162)"
+    imagem.src = "../Exercicio 1/imagens/manhã.jpg"
+}else if (horas >= 13 && horas <= 18) {
+    bodyColor.backgroundColor = "rgba(53,47,73,255)"
+    imagem.src = "../Exercicio 1/imagens/tarde.jpg"
+}else {
+    bodyColor.backgroundColor = "rgba(8,23,30,255)"
+    imagem.src = "../Exercicio 1/imagens/noite.jpg"
+}
